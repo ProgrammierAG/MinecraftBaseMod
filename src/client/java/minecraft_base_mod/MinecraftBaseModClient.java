4932f405df04a1d15e9ef3a154932731bbfdc280
Jemal Schleicher <jemal.schleicher@cgi.com>
2026-03-17 15:00:55 +0100

    feat: initial commit

---
+++ src/client/java/minecraft_base_mod/MinecraftBaseModClient.java
@@ -0,0 +1,10 @@
+package minecraft_base_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftBaseModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
